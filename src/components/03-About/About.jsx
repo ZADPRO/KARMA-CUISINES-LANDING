d2c19b9177711d1b3ed5a71d14af390b8a5dd7a1
IONHAX04 <thirukumara04@gmail.com>
2025-02-01 10:05:17 +0530

    about page drafted

--- src/components/03-About/About.jsx
+++ src/components/03-About/About.jsx
@@ -1,8 +1,8 @@
 import "./About.css";
-import story1 from "../../assets/story/5.jpg";
-import story2 from "../../assets/story/2.jpg";
-import story3 from "../../assets/story/3.jpg";
-import story4 from "../../assets/story/4.jpg";
+import story1 from "../../assets/story/one.jpg";
+import story2 from "../../assets/story/two.jpg";
+import story3 from "../../assets/story/three.jpg";
+import story4 from "../../assets/story/four.jpg";
 
 import { useScroll, useTransform, motion } from "framer-motion";
 import { useEffect, useRef } from "react";
@@ -57,31 +57,32 @@ const Section2 = ({ scrollYProgress }) => {
   return (
     <motion.section
       style={{ scale, rotate }}
-      className="relative lg:h-screen bg-gradient-to-t to-[#1a1919] from-[#06060e] text-white "
+      className="relative lg:h-screen bg-gradient-to-t to-[#1a1919] from-[#06060e] text-white flex items-center justify-center"
     >
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:54px_54px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       <article className="container mx-auto relative z-9 ">
-        <h1 className="text-6xl leading-[100%] py-10 font-semibold  tracking-tight ">
-          Images That doesn't Make any sense <br /> but still in this section
+        <h1 className="text-4xl leading-[100%] py-10 font-semibold  tracking-tight text-center">
+          The Story of Karma Cuisines: Bringing Authentic Exotic Flavors to Your
+          Doorstep{" "}
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           <img
-            src="https://images.unsplash.com/photo-1717893777838-4e222311630b?w=1200&auto=format&fit=crop"
+            src={story1}
             alt="img"
             className=" object-cover w-full rounded-md h-full"
           />
           <img
-            src="https://images.unsplash.com/photo-1717618389115-88db6d7d8f77?w=500&auto=format&fit=crop"
+            src={story2}
             alt="img"
             className=" object-cover w-full rounded-md"
           />
           <img
-            src="https://images.unsplash.com/photo-1717588604557-55b2888f59a6?w=500&auto=format&fit=crop"
+            src={story3}
             alt="img"
             className=" object-cover w-full rounded-md h-full"
           />
           <img
-            src="https://images.unsplash.com/photo-1713417338603-1b6b72fcade2?w=500&auto=format&fit=crop"
+            src={story4}
             alt="img"
             className=" object-cover w-full rounded-md h-full"
           />
